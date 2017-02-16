0494e1f3f8019886e27b164b15be7797014fba52
Sarpreet Singh <sb223ce@student.lnu.se>
2017-02-16 19:30:49 +0100

    removed hard coded 503

--- Lab2/src/http/Server.java
+++ Lab2/src/http/Server.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class Server {
 
 	public final static int PORT = 8080;
-	public final static boolean UNDER_MAINTAINENCE = true;
+	public final static boolean UNDER_MAINTAINENCE = false;
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
