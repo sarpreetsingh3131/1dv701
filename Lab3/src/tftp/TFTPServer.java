cdd79b5685a0e8b4ec4f980c990e7d9941248820
Sarpreet Singh <sb223ce@student.lnu.se>
2017-02-27 17:37:45 +0100

    some confusions

--- Lab3/src/tftp/TFTPServer.java
+++ Lab3/src/tftp/TFTPServer.java
@@ -1,5 +1,7 @@
 package tftp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -7,14 +9,15 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class TFTPServer {
 	public static final int TFTPPORT = 4970;
 	public static final int BUFSIZE = 516;
 
 	// custom address at your PC
-	public static final String READDIR = "src/read";
-	public static final String WRITEDIR = "src/write";
+	public static final String READDIR = "src/read/";
+	public static final String WRITEDIR = "src/write/";
 
 	// OP codes
 	public static final int OP_RRQ = 1;
@@ -69,15 +72,18 @@ public class TFTPServer {
 						// Connect to client
 						sendSocket.connect(clientAddress);
 
-						System.out.printf("%s request for %s from %s using port %d\n",
+						System.out.printf("%s request for %s from %s using port\n",
 								(reqtype == OP_RRQ) ? "Read" : "Write", clientAddress.getHostName(),
 								clientAddress.getPort());
 
 						// Read request
 						if (reqtype == OP_RRQ) {
+
 							requestedFile.insert(0, READDIR);
+
 							HandleRQ(sendSocket, requestedFile.toString(), OP_RRQ);
 						}
+
 						// Write request
 						else {
 							requestedFile.insert(0, WRITEDIR);
@@ -85,6 +91,9 @@ public class TFTPServer {
 						}
 						sendSocket.close();
 					} catch (SocketException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
@@ -101,7 +110,7 @@ public class TFTPServer {
 	 * @param buf
 	 *            (where to store the read data)
 	 * @return socketAddress (the socket address of the client)
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	private InetSocketAddress receiveFrom(DatagramSocket socket, byte[] buf) throws IOException {
 
@@ -127,54 +136,68 @@ public class TFTPServer {
 	 * @return opcode (request type: RRQ or WRQ)
 	 */
 	private int ParseRQ(byte[] buf, StringBuffer requestedFile) {
-		
+
 		// See "TFTP Formats" in TFTP specification for the RRQ/WRQ request
 		// contents
-		ByteBuffer wrap= ByteBuffer.wrap(buf);
-		
+		ByteBuffer wrap = ByteBuffer.wrap(buf);
+
 		short opcode = wrap.getShort();
-		
+
 		requestedFile.append(new String(buf, 2, buf.length - 2));
 		return opcode;
 	}
 
 	/**
-	 * Handles RRQ and WRQ requests 
+	 * Handles RRQ and WRQ requests
 	 * 
-	 * @param sendSocket (socket used to send/receive packets)
-	 * @param requestedFile (name of file to read/write)
-	 * @param opcode (RRQ or WRQ)
+	 * @param sendSocket
+	 *            (socket used to send/receive packets)
+	 * @param requestedFile
+	 *            (name of file to read/write)
+	 * @param opcode
+	 *            (RRQ or WRQ)
+	 * @throws IOException 
+	 * @throws FileNotFoundException
 	 */
-	private void HandleRQ(DatagramSocket sendSocket, String requestedFile, int opcode) 
-	{		
-		if(opcode == OP_RRQ)
-		{
-			// See "TFTP Formats" in TFTP specification for the DATA and ACK packet contents
-			//boolean result = send_DATA_receive_ACK(params);
-		}
-		else if (opcode == OP_WRQ) 
-		{
-			//boolean result = receive_DATA_send_ACK(params);
-		}
-		else 
-		{
+	private void HandleRQ(DatagramSocket sendSocket, String requestedFile, int opcode) throws IOException {
+		String filePath = requestedFile.split("\0")[0];
+		byte[] buffer = new byte[BUFSIZE - 4];
+		int block = 0;
+
+		if (opcode == OP_RRQ) {
+			// See "TFTP Formats" in TFTP specification for the DATA and ACK
+			// packet contents
+			++block;
+			
+			boolean result = send_DATA_receive_ACK(sendSocket, opcode, block);
+			try {
+				FileInputStream stream = new FileInputStream(filePath);
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else if (opcode == OP_WRQ) {
+			block++;
+			// boolean result = receive_DATA_send_ACK(params);
+		} else {
 			System.err.println("Invalid request. Sending an error packet.");
-			// See "TFTP Formats" in TFTP specification for the ERROR packet contents
-		//	send_ERR(params);
+			// See "TFTP Formats" in TFTP specification for the ERROR packet
+			// contents
+			// send_ERR(params);
 			return;
-		}		
+		}
 	}
 
-	/**
-	To be implemented
-	*/
-	/*private boolean send_DATA_receive_ACK(params)
-	{return true;}
+	private boolean send_DATA_receive_ACK(DatagramSocket sendSocket, int opcode, int block) throws IOException {
+		byte[] buffer = new byte[]{(byte)opcode, (byte)block};
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		sendSocket.send(packet);
+		return true;
+	}
 
-	private boolean receive_DATA_send_ACK(params)
-	{return true;}
+	// private boolean receive_DATA_send_ACK(params) {return true;}
 
-	private void send_ERR(params)
-	{}
-	*/
+	// private void send_ERR(params) {}
 }
